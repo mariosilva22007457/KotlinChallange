022ef6fbded5e437bd3f0ccd0ed426e12ae1cc15
Mário Malta da Silva a22007457 <79096482+mariosilva22007457@users.noreply.github.com>
2023-03-25 18:20:04 +0000

    Add files via upload

---
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -0,0 +1,39 @@
+package pt.ulusofona.cm.kotlin.challenge.models
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoDesligadoException
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
+import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
+
+ class Motor(var cavalos: Int, var cilindrada: Int) : Ligavel {
+    var on: Boolean = false
+
+
+
+    override fun ligar() {
+        if(on){
+            throw  VeiculoLigadoException("O veiculo já se encontra ligado")
+        }
+
+        on = true
+
+    }
+
+
+     override fun desligar() {
+         if(!on){
+             throw VeiculoDesligadoException("O veiculo já encontra desligado")
+         }
+         on = false
+     }
+
+     override fun estaLigado(): Boolean {
+        return on
+
+
+     }
+
+     override fun toString(): String {
+         return "Motor | $cavalos | $cilindrada"
+     }
+
+
+ }
